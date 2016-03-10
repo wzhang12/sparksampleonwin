2919cb5adce8b620be2f615da2956976e62b8609
zhangwen <zhangwenhi@sina.com>
2016-03-10 18:28:24 +0800

    add spark stream example

--- src/main/java/easydemo/SparkStreamingDemo.scala
+++ src/main/java/easydemo/SparkStreamingDemo.scala
@@ -16,16 +16,48 @@ object SparkStreamingDemo {
       val lines = ssc.socketTextStream("192.168.137.101", 9998)
       val words = lines.flatMap(_.split(" "))
       val pairs = words.map(word => (word, 1))
-      val wordCounts = pairs.reduceByKey(_ + _)
-      wordCounts.print()
+      /*
+      reduceByKey(binary_function)
+      reduceByKey就是对元素为KV对的RDD中Key相同的元素的Value进行binary_function的reduce操作，因此，Key相同的多个元素的值被reduce为一个值，然后与原RDD中的Key组成一个新的KV对。
+       */
+//      val wordCounts = pairs.reduceByKey(_ + _)
+//      wordCounts.print()
 
       /*
       如何统计总共的每个record的每个单词的出现的次数 用updateStateByKey
+      这个方法对比1.6版本出的mapWithState性能已经差了许多
+       */
+      ssc.checkpoint("D:\\workspace\\tesr2\\sparksampleonwin\\checkpointFile")
+      val updateFunc = (values: Seq[Int], state: Option[Int]) => {
+
+
+        println("=================================",state)
+        val currentCount = values.sum
+        println("=================================c",currentCount)
+        val previousCount = state.getOrElse(0)
+        Some(currentCount + previousCount)
+      }
+
+      pairs.updateStateByKey(updateFunc).print()
+
+      /*
+      transform 一个DStream包含多个rdd，这个方法会将多个rdd应用于传入的方法
+      RDD中的许多方法DStream中是没有的
+      PairDStreamFunctions中是含有join
+      但一般的DStream中不含有join
        */
 
+      /*
+      窗口操作
+      每隔10秒，统计一下过去30秒过来的数据
+      val windowedWordCounts = pairs.reduceByKeyAndWindow(_ + _, Seconds(30), Seconds(10))
+       */
+//      val windowedWordCounts = pairs.reduceByKeyAndWindow((a:Int,b:Int) => (a + b),Seconds(30), Seconds(10))
+//      windowedWordCounts.print();
 
       ssc.start()             // Start the computation
       ssc.awaitTermination()  // Wait for the computation to terminate
       ssc.stop()
     }
+
 }
